ba22e8da4828e5d3194b6c7fedfdd7bc8b6896bc
Misha <misha.saighanov@mail.ru>
2023-08-01 10:34:25 +0600

    add dropdown functional

--- components/layout/nav/nav.tsx
+++ components/layout/nav/nav.tsx
@@ -1,3 +1,4 @@
+"use client";
 
 import "./nav.sass";
 import Link from "next/link";
@@ -5,22 +6,30 @@ import Image from "next/image";
 import chevron from "@/public/Chevron.svg";
 
 import Dropdown from "@/components/ui/dropdown/dropdown";
+import { useState } from "react";
 
 const Nav = () => {
+
+  const [openDropdown, setOpenDropdown] = useState<boolean>(false);
+
+  const chevronClassName = "nav__chevron" + (openDropdown ? " active" : "")
+
   return (
     <div className="nav">
       <Link className="nav__my-subscriptions" href="/">My subscriptions</Link>
-      <div className="nav__user">
+      <div className="nav__user" onClick={() => setOpenDropdown(state => !state)}>
         Alex
         <Image
-          className="nav__chevron"
+          className={chevronClassName}
           src={chevron}
           alt={'chevron'}
         />
       </div>
-      <div className="nav__dropdown">
-        <Dropdown />
-      </div>
+      {openDropdown ? (
+        <div className="nav__dropdown">
+          <Dropdown />
+        </div>
+      ) : null}
     </div>
   )
 }
