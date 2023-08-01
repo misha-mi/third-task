ad7b98d1bb596fb8bbd7b91de17b1852e7b83fa2
Misha <misha.saighanov@mail.ru>
2023-08-01 09:22:57 +0600

    basic header's ui

--- components/layout/header/header.tsx
+++ components/layout/header/header.tsx
@@ -1,14 +1,24 @@
+import Link from "next/link";
+import chevron from "@/public/Chevron.svg";
+import Image from "next/image";
 import "./header.sass";
 import Logo from "@/components/ui/logo/logo";
 
 
 const Header = () => {
   return (
-    <div>
+    <div className="header">
       <Logo />
       <div className="header__wrapper">
-        <a className="header__my-subscriptions" href="/subscriptions">My subscriptions</a>
-        <div className="header__user">Alex</div>
+        <Link className="header__my-subscriptions" href="/">My subscriptions</Link>
+        <div className="header__user">
+          Alex
+          <Image
+            className="header__chevron"
+            src={chevron}
+            alt={'chevron'}
+          />
+        </div>
       </div>
     </div>
   )
