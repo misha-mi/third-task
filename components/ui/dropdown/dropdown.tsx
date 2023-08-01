6175011e0dd43c105a8e7f73aa78535faf4ca3d4
Misha <misha.saighanov@mail.ru>
2023-08-01 09:55:33 +0600

    add dropdown component

---
+++ components/ui/dropdown/dropdown.tsx
@@ -0,0 +1,34 @@
+import "./dropdown.sass";
+import Link from "next/link";
+import Image from "next/image";
+import logout from "@/public/logout.svg";
+import setting from "@/public/setting.svg";
+
+const Dropdown = () => {
+  return (
+    <div className="dropdown">
+      <ul className="dropdown__wrapper">
+        <li>
+          <Link href="/" className="dropdown__item">
+            <Image
+              src={setting}
+              alt={"setting"}
+            />
+            Settings
+          </Link>
+        </li>
+        <li>
+          <Link href="/" className="dropdown__item">
+            <Image
+              src={logout}
+              alt={"logout"}
+            />
+            Logout
+          </Link>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default Dropdown
