0d782db0286f21bc60ea317251e9ede96ee1eb3d
Misha <misha.saighanov@mail.ru>
2023-08-07 09:35:27 +0600

    moving checkout page

--- app/authorization/checkout/page.tsx
+++ app/authorization/checkout/page.tsx
@@ -1,8 +1,31 @@
 
-import Checkout from "@/components/layout/checkout/checkout";
+import "./checkout.sass";
+import Button from "@/components/ui/button/button";
+import Cheque from "@/components/ui/cheque/cheque";
+import Title from "@/components/ui/title/title";
 
-const CheckoutPage = () => {
-  return <Checkout />;
+const Checkout = () => {
+  return (
+    <div className="checkout">
+
+      <Title titleText="Checkout" />
+
+      <div className="checkout__cheque">
+        <Cheque subscription={{ name: "ad", price: "adsf" }} />
+      </div>
+
+      <div className="checkout__total">
+        <p className="checkout__key">Total:</p>
+        <p className="checkout__value">
+          $77
+        </p>
+      </div>
+
+      <div className="checkout__button">
+        <Button text="Purchase" width="w200px" />
+      </div>
+    </div>
+  )
 }
 
-export default CheckoutPage;
+export default Checkout;
