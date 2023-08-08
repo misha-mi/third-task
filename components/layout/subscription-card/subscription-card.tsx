71b4fd72e498caeb4c37c7412b26e74c0f3419a7
Misha <misha.saighanov@mail.ru>
2023-08-08 09:56:13 +0600

    create subscription-card component

---
+++ components/layout/subscription-card/subscription-card.tsx
@@ -0,0 +1,32 @@
+import "./subscription-card.sass";
+
+import Button from "@/components/ui/button/button"
+import Title from "@/components/ui/title/title"
+
+const SubscriptionCard = () => {
+  return (
+    <article className="subscription-card">
+      <header className="subscription-card__header">
+        <div className="subscription-card__gscore">Gscore</div>
+        <div className="subscription-card__status">Active</div>
+      </header>
+
+      <div className="subscription-card__divider"></div>
+
+      <main className="subscription-card__content">
+        <div className="subscription-card__subscription">
+          <Title titleText="Single site license" />
+          <div className="subscription-card__price">$77</div>
+        </div>
+
+        <div className="subscription-card__date">valid until 21.10.2022</div>
+
+        <div className="subscription-card__button">
+          <Button text="View" theme="color100" width="w120px" />
+        </div>
+      </main>
+    </article>
+  )
+}
+
+export default SubscriptionCard;
