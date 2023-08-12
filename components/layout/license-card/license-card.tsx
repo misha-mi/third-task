3f9adcaf4337bbb0504e9bfc0e1a594bbd98e98a
Misha <misha.saighanov@mail.ru>
2023-08-12 15:42:48 +0600

    adaptation license-card

--- components/layout/license-card/license-card.tsx
+++ components/layout/license-card/license-card.tsx
@@ -10,7 +10,7 @@ import { useState } from "react";
 
 const LicenseCard = () => {
 
-  const [status, setStatus] = useState<TStatus>("hold");
+  const [status, setStatus] = useState<TStatus>("active");
 
   return (
     <div className="license-card">
@@ -22,21 +22,23 @@ const LicenseCard = () => {
 
       <span></span>
 
-      <div className="license-card__title">License code</div>
-      <div className="license-card__title">Domain</div>
-      <div className="license-card__title license-card_ml28px">Status</div>
+      <div className="license-card__title license-card__license">License code</div>
+      <div className="license-card__title license-card__domain">Domain</div>
+      <div className="license-card__title license-card_none license-card_ml28px">Status</div>
 
       <input type="text" className="license-card__input" value="sl=ru&tl=en&texte=%D0..." />
 
       <div className="license-card__domain-wrapper">
         <input className="license-card__input license-card__domain" value="https://translate.google.com/?sl=rru&tl=enn&ten&tu&tl=enn&ten&te&t=%D0..." />
         {status === "inactive" ? (
-          <Button text="Activate" theme="color100" width="w120px" />
+          <div className="license-card__button">
+            <Button text="Activate" theme="color100" width="w120px" />
+          </div>
         ) : null
         }
       </div>
 
-      <div className="license-card_ml28px">
+      <div className="license-card_ml28px license-card__status">
         <Status status={status} />
       </div>
 
